a9139d5dac6bfb00a15bdfa00ed78111ec9fdf30
Saksham Ahuja <s29ahuja@uwaterloo.ca>
2023-07-13 16:19:20 -0400

    read status register functionality

--- TagV3.0_U575VGT/Core/Inc/Sensor Inc/max17320.h
+++ TagV3.0_U575VGT/Core/Inc/Sensor Inc/max17320.h
@@ -13,6 +13,8 @@
 
 // Include Files
 #include "stm32u5xx_hal.h"
+#include <stdbool.h>
+#include <util.h>
 
 
 // MAX17320G2 Device Address
@@ -28,6 +30,27 @@
 #define MAX17320_TIMEOUT                100
 #define MAX17320_DATA_SIZE              2
 
+// 8-bit to 16-bit conversion
+#define TO_16_BIT(b1, b2)				((uint16_t)(b2 << 8) | (uint16_t)b1)
+
+
+// Register Structs
+
+typedef struct {
+	bool power_on_reset;
+	bool min_current_alert;
+	bool max_current_alert;
+	bool state_of_charge_change_alert; // Alerts 1% change
+	bool min_voltage_alert;
+	bool min_temp_alert;
+	bool min_state_of_charge_alert;
+	bool max_voltage_alert;
+	bool max_temp_alert;
+	bool max_state_of_charge_alert;
+	bool protection_alert;
+
+} max17320_Reg_Status;
+
 
 
 // Device Struct
@@ -35,14 +58,18 @@ typedef struct __MAX17320_HandleTypeDef {
 
     I2C_HandleTypeDef *i2c_handler;
 
-    uint8_t status[2];
 
+    max17320_Reg_Status status;
 
 } MAX17320_HandleTypeDef;
 
 
 
 HAL_StatusTypeDef max17320_init(MAX17320_HandleTypeDef *dev, I2C_HandleTypeDef *hi2c_device);
+
+HAL_StatusTypeDef max17320_get_status(MAX17320_HandleTypeDef *dev);
+
+
 
 #endif // MAX17320_H
 
--- TagV3.0_U575VGT/Core/Src/Sensor Src/max17320.c
+++ TagV3.0_U575VGT/Core/Src/Sensor Src/max17320.c
@@ -6,16 +6,42 @@
  */
 
 // Include files
+
 #include "max17320.h"
+
+static inline max17320_Reg_Status __statusRegister_from_raw(uint16_t raw) {
+	return (max17320_Reg_Status) {
+        .power_on_reset = _RSHIFT(raw, 1, 1),
+	    .min_current_alert = _RSHIFT(raw, 2, 1),
+	    .max_current_alert = _RSHIFT(raw, 6, 1),
+	    .state_of_charge_change_alert = _RSHIFT(raw, 7, 1),
+	    .min_voltage_alert = _RSHIFT(raw, 8, 1),
+	    .min_temp_alert = _RSHIFT(raw, 9, 1),
+	    .min_state_of_charge_alert = _RSHIFT(raw, 10, 1),
+	    .max_voltage_alert = _RSHIFT(raw, 12, 1),
+	    .max_temp_alert = _RSHIFT(raw, 13, 1),
+	    .max_state_of_charge_alert = _RSHIFT(raw, 14, 1),
+	    .protection_alert = _RSHIFT(raw, 15, 1),
+	};
+}
 
 HAL_StatusTypeDef max17320_init(MAX17320_HandleTypeDef *dev, I2C_HandleTypeDef *hi2c_device) {
     HAL_StatusTypeDef ret = HAL_ERROR;
     dev->i2c_handler = hi2c_device;
 
-    uint8_t raw_data[2] = {0};
+   // Check status
+   ret = max17320_get_status(dev);
 
-    // Read the status register and check for correct value of 0x0002
-    ret = HAL_I2C_Mem_Read(dev->i2c_handler, 0x6C, 0x34, I2C_MEMADD_SIZE_8BIT, (uint8_t*)&raw_data, 2, 1000);
+   return ret;
+}
 
-    return ret;
+HAL_StatusTypeDef max17320_get_status(MAX17320_HandleTypeDef *dev) {
+
+    uint8_t data_buf[2] = {0};
+    
+    HAL_StatusTypeDef ret = HAL_I2C_Mem_Read(dev->i2c_handler, MAX17320_DEV_ADDR, MAX17320_REG_STATUS, I2C_MEMADD_SIZE_8BIT, (uint8_t*)&data_buf, 2, 1000);
+
+    dev->status = __statusRegister_from_raw(TO_16_BIT(data_buf[0], data_buf[1]));
+
+	return ret;
 }
