58d5f1bbb3314d32851d448e9650888f507ee205
kgrewal26 <kaveetgrewal@gmail.com>
2023-08-17 12:09:07 -0400

    back to normal operation mode

--- TagV3.0_U575VGT/Core/Inc/Lib Inc/state_machine.h
+++ TagV3.0_U575VGT/Core/Inc/Lib Inc/state_machine.h
@@ -10,9 +10,9 @@
 
 #include "tx_api.h"
 
-#define IS_SIMULATING true
+#define IS_SIMULATING false
 //Should correspond with the state types enum below
-#define SIMULATING_STATE 1
+#define SIMULATING_STATE 0
 
 //Flags inside of our state machine event flags
 #define STATE_TIMEOUT_FLAG 0x1
